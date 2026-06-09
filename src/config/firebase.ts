86447773d975ffc2311401f2b07c19f9a45ad5e7
alfiansyhptr <alfiaansaputra@yahoo.com>
2026-06-09 10:59:55 +0700

    Update from user request

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getMessaging, isSupported } from "firebase/messaging";
+import { getAnalytics } from "firebase/analytics";
 
 export const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -8,11 +9,13 @@ export const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 // Ensure we only initialize once
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+export const analytics = getAnalytics(app);
 
 // Initialize Messaging
 // Check if supported (e.g. not supported in incognito or Safari < 16.4)
